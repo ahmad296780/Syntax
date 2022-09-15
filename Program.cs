191edd9b2e0b3025766f81cc36ea970bf3fb2e90
ahmad <bha296780@gmail.com>
2022-09-15 12:27:52 -0400

    final

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Syntax_and_Syntax_Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9)? ($"{answer} is less than 9 ") : ($" {answer} is greater than 9");
+            Console.WriteLine(response);
+        }
+    }
+}
